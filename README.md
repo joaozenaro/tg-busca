d40d208acdf148e37ba4310d98d6a39eb40c5fff
João Zenaro <joaoz@email.com>
2024-07-02 13:20:02 +0000

    feat: Multiple projects same folder

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
 # TG Aplicações Busca
 
+## Como executar o projeto
+
+### Codespaces (100% online)
+
+WIP
+
+### Linux + Docker
+1. Clonar o repo
+```console
+git clone git@github.com:joaozenaro/tg-busca.git
+ou
+git clone https://github.com/joaozenaro/tg-busca.git
 ```
-run <folder_name>/
+
+2. Abrir o projeto
+```console
+code tg-busca/
 ```
+
+3. Abrir o container
+$$QuickSearch (ctrl + shift + p) \longrightarrow \text{Dev Containers: Rebuild and Reopen in Container}$$
+
+4. Abrir a workspace dentro do container
+$$QuickSearch (ctrl + shift + p) \longrightarrow \text{File: Open Workspace from File}$$
+Endereço da workspace: `/workspaces/tg-busca/.code-workspace`
+
+> Para adicionar novos projetos é necessario adicionar um novo `path` em `folders` no arquivo `.code-workspace`.
---
+++ cycleDetectionDFS/CycleDetection.java
@@ -0,0 +1,33 @@
+import java.util.List;
+
+public class CycleDetection {
+    public void detectCycle(List<Vertex> vertexList) {
+        for (Vertex v : vertexList) {
+            if (!v.isVisited()) {
+                dfs(v);
+            }
+        }
+    }
+
+    public void dfs(Vertex vertex) {
+        System.out.println("\nDFS on vertex: " + vertex);
+        vertex.setBeingVisited(true);
+
+        for (Vertex v : vertex.getAdjacencyList()) {
+            System.out.println("Visiting the neighbors of vertex: " + vertex + ", vertex " + v);
+
+            if (v.isBeingVisited()) {
+                System.out.println("\n<<There is a backward edge, cycle detected!>>");
+                return;
+            }
+
+            if (!v.isVisited()) {
+                dfs(v);
+            }
+
+            System.out.println("Set vertex " + vertex + " setBeingVisited(false) and setVisited(true)");
+            vertex.setBeingVisited(false);
+            vertex.setVisited(true);
+        }
+    }
+}
